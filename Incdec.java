4669fe3e504ca4c20694a16d449585c160d0a449
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-03 01:27:38 +0530

    Add files via upload

---
+++ Incdec.java
@@ -0,0 +1,12 @@
+public class Incdec {
+    public static void main(String[] args) {
+
+        int x = 5;
+
+        System.out.println("Initial value of x: " + x);
+        System.out.println("Post increment: " + x++);
+        System.out.println("Pre increment: " + ++x);
+        System.out.println("Post decrement: " + x--);
+        System.out.println("Pre decrement: " + --x);
+    }
+}
