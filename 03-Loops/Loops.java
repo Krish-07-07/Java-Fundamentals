9fcd43ea8cdf85317254fbb306be95f6b0843741
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-03 01:54:08 +0530

    Update Loops.java
    Refactor main method to include commented-out loop examples and a do-while loop.

--- 03-Loops/Loops.java
+++ 03-Loops/Loops.java
@@ -1,1 +1,67 @@
 
+public class Loop {
+    static void main(){
+        
+        // for (int i = 1 ; i<=4; i++){
+        //     for (int j = 1 ; j<=4;j++){
+        //      System.out.print("* ");
+        //     }
+        //      System.out.println();    
+        //    }
+
+        // for (int i = 1 ; i <= 3 ; i++){
+        //     for (int j = 1 ; j <= 3; j++){
+        //         System.out.println("i =" + i + " j =" + j);
+        //     }
+
+        // }
+        
+        // for (int i = 1 ; i<=10 ;i++){
+        //     if(i == 5){
+        //         break;
+        //     }
+        //     System.out.println(i);
+        // }
+
+        //  for(int i = 1 ; i<=10 ; i++){
+        //     if(i==5 || i==7){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //  }        
+        
+        // int i = 1;
+        // while (i<=5){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        
+        // int i = 1;
+        // while (i<=10){
+        //     System.out.println("Krish");
+        //     i++;
+        // }
+
+        //  int i=1;
+        //  while(i<=2){
+        //     int j=1;
+        //     while(j<=3){
+        //     System.out.println("i=" + i + "j=" + j);
+        //     j++ ; 
+        //     }
+        //       i++;
+        //  }
+
+         
+          int i = 1 ;
+          do{
+            System.out.println(i);
+            i++;
+          }while(i<=0);
+         
+
+
+
+    }                   
+}
+
