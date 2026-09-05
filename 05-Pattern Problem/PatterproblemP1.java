21f6b3128b86994259c7482cecd6e7899b843e1a
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-05 11:52:37 +0530

    Update PatterproblemP1.java

--- 05-Pattern Problem/PatterproblemP1.java
+++ 05-Pattern Problem/PatterproblemP1.java
@@ -1,1 +1,61 @@
+public class PatternproblemP1 {
 
+    static void main (String[] args){
+      // Square pattern 
+
+        int n = 4 ;
+        for (int row =1 ; row<= n;row++){
+            for (int col =1 ;col<=n;col++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+
+        }
+      
+      // Rectangle pattern
+      
+        // int n = 3 ;
+        //  for (int row = 1 ; row <=n ; row ++){
+        //      for (int col =1 ; col<=5 ; col++){
+        //         System.out.print("* ");
+        //      }
+        //      System.out.println();
+        //  }
+        
+        //Right angle triangle pattern 
+
+        // int n= 5 ;
+        // for(int row = 1; row<=n ; row++){
+        //     for(int col =1 ; col<=row ; col++){
+        //         System.out.print("* ");
+        //         }
+        //         System.out.println();
+        //     }
+
+        //Rohmbus pattern 
+
+        // int n = 5;
+        // for(int row = 1; row<=n ; row++){
+
+        //     for(int col=1 ; col<=n-row ; col++){
+        //         System.out.print("  ");
+        //     }
+
+        //     for(int col=1 ; col<=5;col++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        
+        // }
+
+       //inverted right angle traingle
+
+    //    int n= 5;
+    //    for(int row =1  ; row<= n ;row++){
+    //        for(int col =1  ; col<= n-row+1;col++){
+    //         System.out.print("* ");
+    //        }
+    //        System.out.println();
+    //    }
+    }
+}
