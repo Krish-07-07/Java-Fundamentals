fa117c3c1275470482744f9be9960ae10cddae57
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-07 22:53:29 +0530

    Part 3
    Removed all commented-out code for various patterns.

--- 05-Pattern Problem/PatternproblemP3.java
+++ 05-Pattern Problem/PatternproblemP3.java
@@ -1,1 +1,118 @@
+public class PatternproblemP3.java{
+  public static void main (String[] args){
+    //hollow pyramid 
 
+    // int n = 5 ;
+
+    // for (int row =1 ; row <= n; row ++){
+         
+    //     for(int col =1 ; col<= n - row ;col++){
+    //     System.out.print("  ");
+    //     }
+
+    //     if (row ==1 || row ==n){
+    //         for (int col =1  ; col<= 2*row -1 ; col++){
+    //             System.out.print("* ");
+    //         }
+    //        }
+    //        else {
+
+    //         System.out.print("* ");
+
+    //         for(int col =1 ; col<= 2*row - 3 ; col++){
+    //             System.out.print("  ");
+    //         } 
+
+    //         System.out.print("* ");
+
+    //        }
+
+    //        System.out.println();
+    // }
+
+
+    //Solid diamond pattern
+
+    // int n = 4;
+    
+    // for(int row = 1 ; row <= n ; row ++){
+
+    //    for(int col= 1 ; col<= n-row; col++){
+    //     System.out.print("  ");
+    //    } 
+
+    //    for (int col = 1 ; col <= 2*row -1 ; col++){
+    //     System.out.print("* ");
+    //    }
+    //    System.out.println();
+    // }
+
+
+    //  for (int row =1 ; row<= n ; row ++){
+
+    //     if(row==1){
+    //         continue;
+    //     }
+
+    //     for (int col =1 ; col<=row -1 ; col++){
+    //         System.out.print("  ");
+    //     }
+
+    //     for (int col =1 ; col <= 2*n-2*row+1 ; col++){
+    //         System.out.print("* ");
+    //     }
+    //     System.out.println();
+    //  }
+
+     //hollow diamond pattern 
+
+    //  int n = 4 ;
+
+    // for (int row =1 ; row <= n; row ++){
+         
+    //     for(int col =1 ; col<= n - row ;col++){
+    //     System.out.print("  ");
+    //     }
+
+    //     if (row ==1 ){
+    //         for (int col =1  ; col<= 2*row -1 ; col++){
+    //             System.out.print("* ");
+    //         }
+    //        }
+    //        else {
+
+    //         System.out.print("* ");
+
+    //         for(int col =1 ; col<= 2*row - 3 ; col++){
+    //             System.out.print("  ");
+    //         } 
+
+    //         System.out.print("* ");
+
+    //        }
+
+    //        System.out.println();
+    // }
+
+    //   for (int row =1 ; row <= (n-1) ;row++){
+    //       for (int col = 1 ; col <= row ;col++){
+    //         System.out.print("  ");
+    //       }
+
+    //       if (row==(n-1)){
+    //         System.out.print("* ");
+    //       }
+
+    //       else {
+    //         System.out.print("* ");
+
+    //         for(int col =1 ; col<= 2*(n-row)-3;col++){
+    //             System.out.print("  ");
+    //         }
+    //         System.out.print("* ");
+    //       }
+    //       System.out.println();  
+
+    //   }
+  }
+}
