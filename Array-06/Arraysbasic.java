5cfe2eff0ddb2628c6f9cb3931091fbf1bd95a16
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-12 23:02:47 +0530

    Add files via upload

---
+++ Array-06/Arraysbasic.java
@@ -0,0 +1,25 @@
+public class Arraysbasic {
+    public static void main (String[] args){
+     // Declaration 
+     int arr [];
+     //allocation
+     arr = new int [3];
+     //init
+     int brr [] ={12,15,56};
+
+    //  int n = brr.length;
+    //  for(int index = 0 ; index<=n-1; index++){
+    //     System.out.println(brr[index]);
+    //  }
+
+     // for each loop 
+     for (int value:brr){
+        System.out.println(value);
+     }
+
+
+
+    //  System.out.println("Value at o index :" + brr[0]);
+
+    }
+}
