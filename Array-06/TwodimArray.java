0c886757c836e4e0285337ebb3a6b50f5d5b7b0b
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-23 19:50:21 +0530

    Update TwodimArray.java

--- Array-06/TwodimArray.java
+++ Array-06/TwodimArray.java
@@ -12,16 +12,16 @@ public class TwodimArray {
             {9}
         };
 
-        // int rowLength = brr.length;
-        // //int columnLength = brr[0].length;
+        int rowLength = brr.length;
+        //int columnLength = brr[0].length;
 
-        // for(int rowIndex = 0 ; rowIndex<=rowLength-1 ;rowIndex++){
-        //     int columnLength =brr[rowIndex].length;
-        //     for(int columnIndex = 0 ; columnIndex<= columnLength-1 ; columnIndex++){
-        //         System.out.print(brr[rowIndex][columnIndex]+" ");
-        //     }
-        //     System.out.println();
-        // }
+        for(int rowIndex = 0 ; rowIndex<=rowLength-1 ;rowIndex++){
+            int columnLength =brr[rowIndex].length;
+            for(int columnIndex = 0 ; columnIndex<= columnLength-1 ; columnIndex++){
+                System.out.print(brr[rowIndex][columnIndex]+" ");
+            }
+            System.out.println();
+        }
          
         
         // Traversal in 2 d array 
