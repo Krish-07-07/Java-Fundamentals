6d8e53029cf42b50c868a1563d1e5cc206a58a37
Krish Kumar Gupta <krishgupta6200@gmail.com>
2026-09-24 20:14:42 +0530

    Create Stringsbasics.java

---
+++ 07-Strings/Stringsbasics.java
@@ -0,0 +1,99 @@
+import java.util.Scanner;
+public class StringsBasics {
+    public static void main(String[] args){
+        
+        String name ="Krish";
+        name = name.replace('r','k');
+        System.out.println(name);
+        
+        // String input = "My,Name,is,Krish";
+        // String[] Words = input.split(",");
+        // for(String str: Words){
+        //   System.out.println(str);
+        // }
+
+        // String name = "Krish";
+        // char[] crr = name.toCharArray();
+        // for(char ch: crr){
+        //     System.out.println("Value of char "+ ch);
+        // }
+
+
+        // String name ="My name is Krish";
+        // System.out.println(name.startsWith("My "));
+        // System.out.println(name.endsWith("Krish"));
+
+
+        
+        // int num = 8923;
+        // String str = String.valueOf(num);
+        // System.out.println(num+1);
+        // System.out.println(str+1);
+        
+    
+        // String str = "My name is krish";
+        // System.out.println(str.substring(3,8));
+        // System.out.println(str.contains("krish")); 
+
+
+        // String name = "love";
+        // System.out.println(name.toUpperCase());
+        // String name1 = "KrIsH";
+        // System.out.println(name1.toLowerCase());
+        
+
+
+        //empty --> length = 0
+        //blank --> empty or sirf spaces h string
+
+        // String str = "  ";
+        // System.out.println(str.length());
+        // System.out.println(str.isEmpty());
+        // System.out.println(str.isBlank());
+        // String name = "  Krish   ";
+        // System.out.println(name.length());
+        // name = name.trim();
+        // System.out.println(name.length());
+
+
+        // String str = "Ramesh";
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(3));
+        // String name = "RAMESH";
+        // System.out.println(str.equals(name));
+        // System.out.println(str.equalsIgnoreCase(name));
+
+        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Provide the string content:");
+        // String str = sc.nextLine();
+        // System.out.println("value :" +str);
+
+        // System.out.println("Provide the string conetent:");
+        // String str1 = sc.next();
+        // System.out.println("Value:" + str1);
+
+
+
+
+
+
+        // String name1 = "Krish";
+        // String name2 = "KRish";
+
+        // if(name1.equalsIgnoreCase(name2)){
+        //     System.out.println("Both stirngs are equal");
+        // }
+        // else{
+        //     System.out.println("Both are not equal");
+        // }
+
+
+        // String firstName ="Krish";
+        // String lastName = new String("Gupta");
+        // System.out.println(firstName + " " +lastName);
+        // System.out.println(firstName.length());        
+        // System.out.println(firstName.charAt(3));
+    }
+    
+}
